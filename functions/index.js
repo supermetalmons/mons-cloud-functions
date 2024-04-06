1fe965f1c724e45c8ff8d62920ecd5a15386054a
ivan grachev <ivan@ivan.lol>
2024-04-06 22:38:58 +0300

    sign with a truly secret key

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,7 @@ nacl.util = require('tweetnacl-util');
 
 const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
 const client = new SecretManagerServiceClient();
+const bs58 = require('bs58');
 
 exports.hello = onCall(async (request) => {
 
@@ -16,18 +17,18 @@ exports.hello = onCall(async (request) => {
     const uid = request.auth.uid;
     console.log(`Authenticated call by user: ${uid}`);
 
+    const name = `projects/949028813912/secrets/solana-private-key/versions/latest`;
+    const [version] = await client.accessSecretVersion({name});
+    const privateKeyBase58 = version.payload.data.toString('utf8');
+    const privateKeyUint8 = bs58.decode(privateKeyBase58);
+
     const message = `Hello from Firebase, user ${uid}!`;
-    const keyPair = nacl.sign.keyPair();
+    const keyPair = nacl.sign.keyPair.fromSecretKey(privateKeyUint8);
     const messageUint8 = nacl.util.decodeUTF8(message);
     const signedMessage = nacl.sign(messageUint8, keyPair.secretKey);
-
-    const name = `projects/${process.env.GCLOUD_PROJECT}/secrets/solana-private-key/versions/latest`;
-    const [version] = await client.accessSecretVersion({name});
-    const payload = version.payload.data.toString('utf8');
 
     return { 
         message: message,
         signedMessage: nacl.util.encodeBase64(signedMessage),
-        testSecretKey: payload,
       };
 });
