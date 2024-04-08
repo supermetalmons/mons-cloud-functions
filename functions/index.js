6e830cc6c03940cc6dbaa350bcf984560a6a9cf1
ivan grachev <ivan@ivan.lol>
2024-04-08 22:28:25 +0300

    check winner using status

--- functions/index.js
+++ functions/index.js
@@ -11,42 +11,51 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 exports.gameResult = onCall(async (request) => {
-    if (!request.auth) {
-      throw new HttpsError("unauthenticated", "The function must be called while authenticated.");
-    }
-
+    if (!request.auth) { throw new HttpsError("unauthenticated", "The function must be called while authenticated."); }
     const signatureType = request.data.signature;
-    if (signatureType != "ed25519") {
-      throw new HttpsError("invalid-argument", "Unsupported signature type");
-    }
+    if (signatureType != "ed25519") { throw new HttpsError("invalid-argument", "Unsupported signature type"); }
 
     const uid = request.auth.uid;
     const id = request.data.id;
 
-    console.log(`Authenticated call by user: ${uid}`);
-
     const matchRef = admin.database().ref(`players/${uid}/matches/${id}`);
-    const snapshot = await matchRef.once('value');
-    const matchData = snapshot.val();
-    console.log(`Got match data: ${matchData}`);
+    const matchSnapshot = await matchRef.once('value');
+    const matchData = matchSnapshot.val();
 
-    const mons = await import('mons-rust');
-    const rustOutput = mons.greet("world");
+    const inviteRef = admin.database().ref(`invites/${id}`);
+    const inviteSnapshot = await inviteRef.once('value');
+    const inviteData = inviteSnapshot.val();
 
+    const opponentId = inviteData.guestId === uid ? inviteData.hostId : inviteData.guestId;
+
+    const opponentMatchRef = admin.database().ref(`players/${opponentId}/matches/${id}`);
+    const opponentMatchSnapshot = await opponentMatchRef.once('value');
+    const opponentMatchData = opponentMatchSnapshot.val();
+
+    var result = "none"; // gg / win / none / draw
+    var resultForChain = "none";
+    if (matchData.status == "surrendered") {
+      result = "gg";
+      resultForChain = `${id}+${opponentId}`;
+    } else if (opponentMatchData.status == "surrendered") {
+      result = "win";
+      resultForChain = `${id}+${uid}`;
+    } else {
+      const mons = await import('mons-rust');
+      const rustOutput = mons.greet("world"); // TODO: use mons-rust to validate fen
+    }
+    
     const name = `projects/${process.env.GCLOUD_PROJECT}/secrets/solana-private-key/versions/latest`;
     const [version] = await client.accessSecretVersion({name});
     const privateKeyBase58 = version.payload.data.toString('utf8');
     const privateKeyUint8 = bs58.decode(privateKeyBase58);
-
-    const message = `Hello from Firebase, user ${uid}!`;
     const keyPair = nacl.sign.keyPair.fromSecretKey(privateKeyUint8);
-    const messageUint8 = nacl.util.decodeUTF8(message);
-    const signedMessage = nacl.sign(messageUint8, keyPair.secretKey);
+    const messageUint8 = nacl.util.decodeUTF8(resultForChain);
+    const signed = nacl.sign(messageUint8, keyPair.secretKey);
 
     return {
-        result: "none", // TODO: gg / win / none / draw
-        signedMessage: bs58.encode(signedMessage),
-        fen: matchData.fen,
-        rustOutput: rustOutput
+        result: result,
+        resultForChain: resultForChain,
+        signed: bs58.encode(signed),
       };
 });
