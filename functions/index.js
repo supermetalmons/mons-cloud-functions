4509b12584f5f1514bb275a0f4edd38815de09f8
ivan grachev <ivan@ivan.lol>
2024-04-12 17:23:12 +0300

    partially sign claim tx

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,10 @@
 const {onCall, HttpsError} = require("firebase-functions/v2/https");
 const {logger} = require("firebase-functions/v2");
 
-const nacl = require('tweetnacl');
-nacl.util = require('tweetnacl-util');
-
-const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
+const { TransactionInstruction, Keypair, PublicKey, Transaction } = require('@solana/web3.js');
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+const { Buffer } = require('buffer');
+const { BN } = require('bn.js');
 const client = new SecretManagerServiceClient();
 const bs58 = require('bs58');
 const admin = require('firebase-admin');
@@ -72,14 +72,57 @@ exports.gameResult = onCall(async (request) => {
     const [version] = await client.accessSecretVersion({name});
     const privateKeyBase58 = version.payload.data.toString('utf8');
     const privateKeyUint8 = bs58.decode(privateKeyBase58);
-    const keyPair = nacl.sign.keyPair.fromSecretKey(privateKeyUint8);
-    const messageUint8 = nacl.util.decodeUTF8(resultForChain);
-    const signed = nacl.sign(messageUint8, keyPair.secretKey);
+
+    const keyPair = Keypair.fromSecretKey(privateKeyUint8);
+
+    // TODO: use correct addresses
+    // TODO: only respond with a tx to a winner
+
+    const transaction = new Transaction({
+      recentBlockhash: request.data.params.recentBlockhash,
+      feePayer: new PublicKey('7YAH3GzfkZiEuESsYPccNf8u4kExNXQjx9yzUmeyM8Fv'),
+    });
+
+    const gameID = convertBase62StringToBN(id);
+    const seeds = [Buffer.from('game'), Buffer.from(new BN(gameID).toArrayLike(Buffer, 'le', 8))];
+    const [gamePDA, bump] = await PublicKey.findProgramAddress(seeds, new PublicKey('23pPB7HdhdLukP8HxKSDoaSgrf6ESunhQMTCYm9DkJNp'));
+
+    const endGameIx = new TransactionInstruction({
+      keys: [
+        {pubkey: gamePDA, isSigner: false, isWritable: true},
+        {pubkey: new PublicKey('7YAH3GzfkZiEuESsYPccNf8u4kExNXQjx9yzUmeyM8Fv'), isSigner: true, isWritable: true},
+        {pubkey: keyPair.publicKey, isSigner: true, isWritable: false}
+      ],
+      programId: new PublicKey('23pPB7HdhdLukP8HxKSDoaSgrf6ESunhQMTCYm9DkJNp'),
+      data: Buffer.alloc(0)
+    });
+    transaction.add(endGameIx);
+
+    transaction.partialSign(keyPair);
+    const serializedTransaction = transaction.serialize({
+      requireAllSignatures: false
+    });
 
     return {
         remoteCaller: request.data.params.caller,
         result: result,
         resultForChain: resultForChain,
-        signed: bs58.encode(signed),
+        signed: serializedTransaction.toString('base64'),
       };
 });
+
+
+function convertBase62StringToBN(str) {
+  const base62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let result = new BN(0);
+
+  for (let char of str) {
+      const value = base62.indexOf(char);
+      if (value === -1) {
+          throw new Error(`Invalid character in string: ${char}`);
+      }
+      result = result.mul(new BN(62)).add(new BN(value));
+  }
+
+  return result;
+}
