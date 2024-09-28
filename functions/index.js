a92b7ac56121957c02216ea28a7311134d0b10f4
ivan grachev <ivan@ivan.lol>
2024-09-28 20:59:26 +0300

    validate nonce and statement

--- functions/index.js
+++ functions/index.js
@@ -15,22 +15,17 @@ exports.verifyEthAddress = onCall(async (request) => {
   const siweMessage = new SiweMessage(message);
   const fields = await siweMessage.verify({signature});
 
+  const address = fields.data.address;
   // TODO: associate an address with a user if it's ok
 
-  if (fields.success) {
-    const statement = fields.data.statement;
+  if (fields.success && fields.data.nonce === request.auth.uid && fields.data.statement === "mons ftw") {
     return {
       ok: true,
-      message: message, // TODO: remove tmp mirror
-      signature: signature, // TODO: remove tmp mirror
-      statement: statement, // TODO: remove tmp mirror
+      address: address,
     };
   } else {
     return {
       ok: false,
-      message: message, // TODO: remove tmp mirror
-      signature: signature, // TODO: remove tmp mirror
-      wrongData: fields.data.toMessage(), // TODO: remove tmp mirror
     };
   }
 });
