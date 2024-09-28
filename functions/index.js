cfc69997c2f561f0210cfcccc470a0436dd7c1b2
ivan grachev <ivan@ivan.lol>
2024-09-28 20:42:46 +0300

    stub siwe verification

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const {onCall, HttpsError} = require("firebase-functions/v2/https");
 const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+const { SiweMessage } = require('siwe');
+
 const client = new SecretManagerServiceClient();
 const admin = require('firebase-admin');
 admin.initializeApp();
@@ -9,12 +11,26 @@ exports.verifyEthAddress = onCall(async (request) => {
 
   const message = request.data.message;
   const signature = request.data.signature;
-  
-  // TODO: verify, associate an address with a user
 
-  return {
-    ok: true,
-    message: message, // TODO: remove tmp mirror
-    signature: signature, // TODO: remove tmp mirror
-  };
+  const siweMessage = new SiweMessage(message);
+  const fields = await siweMessage.verify({signature});
+
+  // TODO: associate an address with a user if it's ok
+
+  if (fields.success) {
+    const statement = fields.data.statement;
+    return {
+      ok: true,
+      message: message, // TODO: remove tmp mirror
+      signature: signature, // TODO: remove tmp mirror
+      statement: statement, // TODO: remove tmp mirror
+    };
+  } else {
+    return {
+      ok: false,
+      message: message, // TODO: remove tmp mirror
+      signature: signature, // TODO: remove tmp mirror
+      wrongData: fields.data.toMessage(), // TODO: remove tmp mirror
+    };
+  }
 });
