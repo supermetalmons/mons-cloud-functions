5af06bc6dd11dd73a144f9cc346809e7b39a5f6a
ivan grachev <ivan@ivan.lol>
2024-09-29 13:21:31 +0300

    fix eth address logic

--- functions/index.js
+++ functions/index.js
@@ -1,36 +1,45 @@
-const {onCall, HttpsError} = require("firebase-functions/v2/https");
-const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
-const { SiweMessage } = require('siwe');
+const { onCall, HttpsError } = require("firebase-functions/v2/https");
+const { SecretManagerServiceClient } = require("@google-cloud/secret-manager");
+const { SiweMessage } = require("siwe");
 
 const client = new SecretManagerServiceClient();
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.verifyEthAddress = onCall(async (request) => {
-  if (!request.auth) { throw new HttpsError("unauthenticated", "The function must be called while authenticated."); }
+  if (!request.auth) {
+    throw new HttpsError(
+      "unauthenticated",
+      "The function must be called while authenticated."
+    );
+  }
 
   const message = request.data.message;
   const signature = request.data.signature;
 
   const siweMessage = new SiweMessage(message);
-  const fields = await siweMessage.verify({signature});
+  const fields = await siweMessage.verify({ signature });
   const address = fields.data.address;
   const uid = request.auth.uid;
-  
-  const db = admin.database();
-  const ethAddressRef = db.ref(`players/${uid}/ethAddress`);
-  const ethAddressSnapshot = await ethAddressRef.once('value');
-  const existingEthAddress = ethAddressSnapshot.val();
 
-  let responseAddress;
-  if (existingEthAddress === null) {
-    await ethAddressRef.set(address);
-    responseAddress = address;
-  } else {
-    responseAddress = existingEthAddress;
-  }
+  if (
+    fields.success &&
+    fields.data.nonce === uid &&
+    fields.data.statement === "mons ftw"
+  ) {
+    const db = admin.database();
+    const ethAddressRef = db.ref(`players/${uid}/ethAddress`);
+    const ethAddressSnapshot = await ethAddressRef.once("value");
+    const existingEthAddress = ethAddressSnapshot.val();
 
-  if (fields.success && fields.data.nonce === uid && fields.data.statement === "mons ftw") {
+    let responseAddress;
+    if (existingEthAddress === null) {
+      await ethAddressRef.set(address);
+      responseAddress = address;
+    } else {
+      responseAddress = existingEthAddress;
+    }
+
     return {
       ok: true,
       address: responseAddress,
