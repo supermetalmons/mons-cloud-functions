8e1f0a96c8d78e5a93e94005bb36e433e77af62b
ivan grachev <ivan@ivan.lol>
2024-09-30 20:09:12 +0300

    fix signing attestations

--- functions/index.js
+++ functions/index.js
@@ -18,7 +18,13 @@ exports.attestVictory = onCall(async (request) => {
     SchemaEncoder,
   } = require("@ethereum-attestation-service/eas-sdk");
 
-  const eas = new EAS("0xF095fE4b23958b08D38e52d5d5674bBF0C03cbF6");
+  const { ethers } = require("ethers");
+  const privateKey = ethers.Wallet.createRandom().privateKey; // TODO: get secret mons attester key
+  const provider = new ethers.JsonRpcProvider("https://mainnet.base.org");
+  const signer = new ethers.Wallet(privateKey, provider);
+
+  const eas = new EAS("0x4200000000000000000000000000000000000021");
+  eas.connect(signer);
   const delegated = await eas.getDelegated();
   const schemaEncoder = new SchemaEncoder("uint64 gameId, uint64 points, bool isWin");
 
@@ -33,65 +39,64 @@ exports.attestVictory = onCall(async (request) => {
     { name: "isWin", value: false, type: "bool" },
   ]);
 
-  const { ethers } = require("ethers");
-  const privateKey = ethers.Wallet.createRandom().privateKey; // TODO: get secret mons attester key
-  const provider = new ethers.JsonRpcProvider("https://mainnet.base.org");
-  const signer = new ethers.Wallet(privateKey, provider);
+  try {
+    const response1 = await delegated.signDelegatedAttestation(
+      {
+        schema:
+          "0xb6cdeca57cf4618b9e6f619771b9ca43febd99de294a8de229aa4938405f2efa",
+        recipient: address1,
+        expirationTime: 0n,
+        revocable: false,
+        refUID:
+          "0x0000000000000000000000000000000000000000000000000000000000000000",
+        data: encodedData1,
+        deadline: 0n,
+        value: 0n,
+      },
+      signer
+    );
+    const signature1 = response1.signature; 
+    
+    const response2 = await delegated.signDelegatedAttestation(
+      {
+        schema:
+          "0xb6cdeca57cf4618b9e6f619771b9ca43febd99de294a8de229aa4938405f2efa",
+        recipient: address2,
+        expirationTime: 0n,
+        revocable: false,
+        refUID:
+          "0x0000000000000000000000000000000000000000000000000000000000000000",
+        data: encodedData2,
+        deadline: 0n,
+        value: 0n,
+      },
+      signer
+    );
+    const signature2 = response2.signature; 
 
-  const response1 = await delegated.signDelegatedAttestation(
-    {
-      schema:
-        "0xb6cdeca57cf4618b9e6f619771b9ca43febd99de294a8de229aa4938405f2efa",
-      recipient: address1,
-      expirationTime: 0n,
-      revocable: false,
-      refUID:
-        "0x0000000000000000000000000000000000000000000000000000000000000000",
-      data: encodedData1,
-      deadline: 0n,
-      value: 0n,
-    },
-    signer
-  );
-  const signature1 = response1.signature; 
-  
-  const response2 = await delegated.signDelegatedAttestation(
-    {
-      schema:
-        "0xb6cdeca57cf4618b9e6f619771b9ca43febd99de294a8de229aa4938405f2efa",
-      recipient: address2,
-      expirationTime: 0n,
-      revocable: false,
-      refUID:
-        "0x0000000000000000000000000000000000000000000000000000000000000000",
-      data: encodedData2,
-      deadline: 0n,
-      value: 0n,
-    },
-    signer
-  );
-  const signature2 = response2.signature; 
+    const signatures = [
+      {
+        r: signature1.r,
+        s: signature1.s,
+        v: signature1.v
+      },
+      {
+        r: signature2.r,
+        s: signature2.s,
+        v: signature2.v
+      }
+    ];
 
-  const signatures = [
-    {
-      r: signature1.r,
-      s: signature1.s,
-      v: signature1.v
-    },
-    {
-      r: signature2.r,
-      s: signature2.s,
-      v: signature2.v
-    }
-  ];
+    // TODO: add extra data needed for tx to the response
 
-  // TODO: add extra data needed for tx to the response
-
-  return {
-    gameId: gameId, // TODO: remove tmp mirror
-    signatures: signatures,
-    ok: true,
-  };
+    return {
+      signatures: signatures,
+      ok: true,
+    };
+  } catch (error) {
+    console.error("Error in attestVictory:", error);
+    throw new HttpsError("internal", "An error occurred while processing the attestation.");
+  }
 });
 
 exports.verifyEthAddress = onCall(async (request) => {
