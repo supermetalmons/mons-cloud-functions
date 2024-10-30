c4bcc026b32f761e3c78cd20534153d5a8a8e378
ivan grachev <ivan@ivan.lol>
2024-10-30 23:52:58 +0300

    remove legacy format functions

--- functions/index.js
+++ functions/index.js
@@ -2,18 +2,10 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 const { verifyEthAddress } = require("./verifyEthAddress");
-
-const { attestVictory } = require("./attestVictory");
-const { startTimer, claimVictoryByTimer } = require("./timers");
-
 const { attestMatchVictory } = require("./attestMatchVictory");
 const { startMatchTimer, claimMatchVictoryByTimer } = require("./matchTimers");
 
 exports.verifyEthAddress = verifyEthAddress;
-exports.attestVictory = attestVictory;
-exports.startTimer = startTimer;
-exports.claimVictoryByTimer = claimVictoryByTimer;
-
 exports.attestMatchVictory = attestMatchVictory;
 exports.startMatchTimer = startMatchTimer;
 exports.claimMatchVictoryByTimer = claimMatchVictoryByTimer;
